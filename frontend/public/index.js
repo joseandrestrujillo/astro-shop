efe74905d57b9e328153dbc06a7d47a996470272
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-09-16 21:12:02 +0200

    refactor: :necktie: separate the login button logic into a component

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -3,11 +3,6 @@
 async function main() {
     let loginBtn = document.querySelector('#login-btn')
 
-    loginBtn.addEventListener('click', _ => {
-        let loginDialog = document.querySelector('#login-dialog')
-        loginDialog.showModal()
-    })
-
     let loginUsernameInput = document.querySelector('#login-username-input')
     let loginConfirmBtn = document.querySelector('#login-confirm-btn')
 
