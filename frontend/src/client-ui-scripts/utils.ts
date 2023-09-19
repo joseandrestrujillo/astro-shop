2c8d5a6b9b932104caefc1275d3ae81bfb24f86d
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-09-20 00:45:24 +0200

    refactor: render products client side

--- frontend/src/client-ui-scripts/utils.ts
+++ frontend/src/client-ui-scripts/utils.ts
@@ -7,3 +7,13 @@ export function setOnClickHandle (querySelector: string, callback: (event: Mouse
     throw new Error('There are not elements that matches with ' + querySelector)
   }
 }
+
+export function addChild (querySelector: string, element: HTMLElement): void {
+  const parent: HTMLElement | null = document.querySelector(querySelector)
+
+  if (parent != null) {
+    parent.appendChild(element)
+  } else {
+    throw new Error('There are not elements that matches with ' + querySelector)
+  }
+}
