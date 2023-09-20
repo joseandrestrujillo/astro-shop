96660f8be11eb0ee9a554855e2273a5d36a60a1a
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-09-20 19:57:22 +0200

    feat: add new filters in the updateProducts function

--- frontend/src/services/updateProducts.ts
+++ frontend/src/services/updateProducts.ts
@@ -1,3 +1,6 @@
+import { filterCategory } from '../stores/filterCategory'
+import { filterStars } from '../stores/filterStars'
+import { filterPrice } from '../stores/price'
 import { allProducts, products } from '../stores/products'
 import { searchQuery } from '../stores/search'
 import { fetchProducts } from './fetchProducts'
@@ -13,5 +16,21 @@ export async function updateProducts (): Promise<void> {
   if (query != null) {
     cachedProducts = cachedProducts.filter(product => product.title.toLowerCase().includes(query.toLowerCase()))
   }
+
+  const price = filterPrice.get()
+  if (price != null) {
+    cachedProducts = cachedProducts.filter(product => product.price >= price[0] && product.price <= price[1])
+  }
+
+  const category = filterCategory.get()
+  if (category != null) {
+    cachedProducts = cachedProducts.filter(product => product.category === category)
+  }
+
+  const stars = filterStars.get()
+  if (stars != null) {
+    cachedProducts = cachedProducts.filter(product => product.stars >= stars)
+  }
+
   products.set(cachedProducts)
 }
